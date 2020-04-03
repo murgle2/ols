506bb32d235a4c73cefe74ff7f5397a83a3ef81e
murgle2 <yetterbrian@gmail.com>
2020-04-02 21:18:03 -0400

    Remove '#' from url route

--- Client/src/main.js
+++ Client/src/main.js
@@ -1,32 +1,30 @@
-import Vue from "vue";
+import Vue from 'vue';
 import Router from 'vue-router';
-import App from "./App.vue";
+import App from './App.vue';
 
-import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-import Stream from "./components/Stream";
-import About from "./components/About";
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import Stream from './components/Stream';
+import About from './components/About';
 
 Vue.use(Router);
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
 Vue.config.productionTip = false;
 
-
 const routes = [
   { path: '/', component: Stream },
-  { path: '/about', component: About}
+  { path: '/about', component: About }
 ];
 
 const router = new Router({
-  routes
+  routes,
+  mode: 'history'
 });
-
-
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount("#app", );
+}).$mount('#app');
